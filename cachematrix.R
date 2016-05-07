2f2cc7c3893f0b9c97da242d930a5aaed5dc8606
Xsess <snyder.xander@mykolab.com>
2016-05-07 13:53:26 -0700

    Removed coursera comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## Creates "special" matrix object (list) that caches inverse
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
